47a7f6ff16fe43191488726332cb07415bbbff6d
Agustin26l <agustin.lopez2606@gmail.com>
2024-08-06 09:25:15 -0500

    feat: Correción en integración

--- algoritmo.py
+++ algoritmo.py
@@ -5,28 +5,26 @@ palabras = ["python", "java", "javascript", "ruby", "php", "html", "css", "cshar
 def elegir_palabra(lista_palabras):
     return random.choice(lista_palabras)
 
+def mostrar_progreso(palabra, letras_adivinadas):
+    progreso = (letra if letra in letras_adivinadas else "_" for letra in palabra)
+    return "".join(progreso)
+
 def juego_ahorcado():
     palabra = elegir_palabra(palabras)
     letras_adivinadas = set()
     intentos_fallidos = 0
     max_intentos = 6
 
-def mostrar_progreso(palabra, letras_adivinas):
-    progreso = (letra if letra in letras_adivinas else "_" for letra in palabra)
-    return "".join(progreso)
-
-    print("¡Bienvenido al juego juego del Ahorcado!")
+    print("¡Bienvenido al juego del Ahorcado!")
     print(f"Tienes {max_intentos} intentos para adivinar la palabra.")
 
-
     while intentos_fallidos < max_intentos:
-        print(mostrar_progreso(palabra, letras_adivinas))
-        letra = input("Adivina una letra:").lower()
+        print(mostrar_progreso(palabra, letras_adivinadas))
+        letra = input("Adivina una letra: ").lower()
 
-        letras_adivinas.add(letra)
+        letras_adivinadas.add(letra)
         intentos_fallidos += 1
         print(f"Letra incorrecta. Te quedan {max_intentos - intentos_fallidos} intentos.")
 
     if intentos_fallidos == max_intentos:
-        print(F"Lo siento, has perdido. La palabra era: {palabra}")
-
+        print(f"Lo siento, has perdido. La palabra era: {palabra}")
